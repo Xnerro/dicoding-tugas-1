1dfdd6b6501b155ddc46e4c8f71f524205952867
bagus <bagusssyamsu@gmail.com>
2022-07-15 07:51:00 +0700

    first commit

---
+++ asset/index.js
@@ -0,0 +1,17 @@
+document.getElementById('menu').addEventListener('click', () => {
+    let x = document.querySelector('.drop-down');
+    if (x.style.right === '-5rem') {
+        x.style.right = '0px';
+        x.style.opacity = '1';
+    } else {
+        x.style.right = '-5rem';
+        x.style.opacity = '0';
+    }
+});
+
+const x = localStorage.getItem('name');
+
+if (x === null) {
+    alert('please fill out the guest book first');
+    location.assign('/register.html');
+}
